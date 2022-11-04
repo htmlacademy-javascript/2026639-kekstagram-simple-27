30f2aeddee7af34d933085e0c4b9bdcc0b859b88
Keks <keks@htmlacademy.ru>
2022-11-04 21:35:46 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/constants.js
@@ -0,0 +1,39 @@
+export const PHOTO_DESCRIPTIONS = [
+  'Have try new recipe today',
+  'Today\'s car repair routine',
+  'What new film should I watch today?',
+  'Old friends meet',
+  'Hello Georgia!',
+  'Hello Kazakhstan!',
+  'Stop war!',
+  'Where to find the churchhela in Uralsk?',
+  'Thank to Kazakhs for the hospitality so much!',
+  'Goodbye russia!',
+  'I want back to 2020!',
+  'What is better RB or JZ?',
+  'My first shot on my new Sony a9000!',
+  'Kodak Portra is my favourite film',
+  'Look at this skintone!',
+  'My next travel target',
+  'G\'day day from parrots on my terrace!',
+  '７１の曜日',
+  'こんにちわ',
+  'Спасибо HTML Academy за крутой курс!',
+  'Noting is better than vegimite on toast',
+  'One way ticket',
+  'Kerri Chandler\'s Atmosphere track on repeat this week',
+  'New set of 45\'s',
+  'My Gibson Les Paul Custom 1985 for sale',
+];
+
+export const PHOTO_DESCRIPTIONS_COUNT = 25;
+
+export const LIKES_COUNT = {
+  MIN: 15,
+  MAX: 200
+};
+
+export const COMMENTS_COUNT = {
+  MIN: 0,
+  MAX: 200
+};
